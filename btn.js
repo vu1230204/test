2a6aedc0b066a6dab7f3d29f77c38841788436fa
Chaien Không Mập <chaienkhongmap@192.168.1.14>
2024-10-27 22:19:54 +0700

    project

---
+++ btn.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // Function to show the popup when a 'Show' button is clicked
+    $('.show-btn').on('click', function() {
+        // Get data attributes from the button
+        let id = $(this).data('id');
+        let name = $(this).data('name');
+        let group = $(this).data('group');
+        
+        // Set the information in the popup
+        $('#info').html(`ID: ${id}<br>Name: ${name}<br>Group: ${group}`);
+        
+        // Show the popup
+        $('#popup').fadeIn();
+    });
+    
+    // Function to close the popup when the 'OK' button is clicked
+    $('#close-btn').on('click', function() {
+        $('#popup').fadeOut();
+    });
+});
